3549ceb10a215b0c8d69a6e1c55373bf89cca3be
Harry S <harry@LDN-LAP-997576.corp.com>
2026-05-27 09:29:06 +0100

    feat: implement tf-atom-vpc-endpoint-service-aws resource

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,23 @@ output "enabled" {
   description = "Whether the module is enabled."
   value       = local.enabled
 }
+
+output "id" {
+  description = "The ID of the VPC endpoint service."
+  value       = try(aws_vpc_endpoint_service.default[0].id, "")
+}
+
+output "arn" {
+  description = "The ARN of the VPC endpoint service."
+  value       = try(aws_vpc_endpoint_service.default[0].arn, "")
+}
+
+output "service_name" {
+  description = "The service name for consumers."
+  value       = try(aws_vpc_endpoint_service.default[0].service_name, "")
+}
+
+output "state" {
+  description = "The state of the VPC endpoint service."
+  value       = try(aws_vpc_endpoint_service.default[0].state, "")
+}
